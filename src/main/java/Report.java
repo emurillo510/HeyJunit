d977b2dd009fd426658f29e0d8fd1fc6b84c1f05
Elton Murillo <emurillo510@gmail.com>
2017-02-06 09:26:18 -0800

    Adding example.

---
+++ src/main/java/Report.java
@@ -0,0 +1,51 @@
+/**
+ * Created by elton on 2/6/17.
+ */
+public class Report {
+
+    private String id;
+    private String sales;
+    private String qty;
+    private String staffName;
+    private String date;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSales() {
+        return sales;
+    }
+
+    public void setSales(String sales) {
+        this.sales = sales;
+    }
+
+    public String getQty() {
+        return qty;
+    }
+
+    public void setQty(String qty) {
+        this.qty = qty;
+    }
+
+    public String getStaffName() {
+        return staffName;
+    }
+
+    public void setStaffName(String staffName) {
+        this.staffName = staffName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
--- src/test/java/HelloServiceTest.java
+++ src/test/java/HelloServiceTest.java
@@ -1,15 +1,30 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.batch.test.MetaDataInstanceFactory;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by elton on 2/6/17.
  */
+
+@RunWith(SpringJUnit4ClassRunner.class) //Indicates that the class should use Spring's JUnit facilities
+@ContextConfiguration(locations = {
+  "/flatfilereader-batch-context.xml"
+})
 public class HelloServiceTest {
 
     HelloService service;
+
+    @Autowired
+    FlatFileItemReader reader;
 
     @Before
     public void setUp(){
@@ -23,10 +38,31 @@ public class HelloServiceTest {
 
     @Test
     public void sayHelloTest(){
+        /*
         final String expected = "Hello, World!";
 
         assertEquals("sayHelloTest: ", this.service.sayHello(), expected);
+        */
 
+    }
+
+    @Test
+    public void batchTest() throws Exception {
+
+        reader.setResource(new FileSystemResource("input/report.csv"));
+
+        reader.open(MetaDataInstanceFactory.createJobExecution().getExecutionContext());
+
+        final Report expected = new Report();
+        expected.setId("0");
+        expected.setQty("1");
+        expected.setSales("Tech");
+        expected.setStaffName("Elton");
+        expected.setDate("2017");
+
+        final Report actual = (Report) this.reader.read();
+
+        assertEquals("batchTest: ", actual.getStaffName(), expected.getStaffName());
     }
 
 }
